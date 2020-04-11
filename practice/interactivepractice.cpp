5353967317c7cb90dd02eae0563bffc1a6701ad5
Manzood <manzood.naqvi@gmail.com>
2020-04-11 22:39:05 +0530

    Small update

---
+++ practice/interactivepractice.cpp
@@ -0,0 +1,24 @@
+#include<cstdio>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+int main () {
+	int t;
+	cin>>t;
+	while (t--) {
+		int a,b;
+		cin>>a>>b;
+		int low=a,high=b;
+		bool found=false;
+		while(!found) {
+			q=low+high;
+			q/=2;
+			cout<<q<<endl;
+			if (q==)
+		}
+	}
+}
