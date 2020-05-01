fddb3529d7c2b759b5c0f7b5efe2c3555e280b57
Manzood <manzood.naqvi@gmail.com>
2020-05-01 10:40:19 +0530

    Added the solution of Problem A in Code Jam Round 1B

--- 2020/Round 1/B/a.cpp
+++ 2020/Round 1/B/a.cpp
@@ -1,183 +1,117 @@
 #include<cstdio>
 #include<algorithm>
-#include<iostream>
 #include<vector>
 #include<set>
-#include<map>
+#include<iostream>
+
 using namespace std;
 #define debug(x) cout<<#x<<" = "<<x<<endl;
 
-vector <int> getbin (int n) {
-	vector <int> bin;
-	int size=0;
-	while (n>0) {
-		if (n%2==1) {
-			bin.push_back(1);
-		}
-		else {
-			bin.push_back(0);
-		}
-		n/=2;
-		size++;
+bool check(int x, int y) {
+	if (x==0 && y==0) {
+		return true;
 	}
-	return bin;
-}
-
-vector <int> getnegative(int num) {
-	int temp=1;
-	int count=1;
-	while (temp<num) {
-		temp*=2;
-		count++;
-	}
-	int diff=temp-num;
-	vector <int> neg=getbin(diff);
-	vector <int> res(count);
-	for (int i=0;i<neg.size();i++) {
-		if (neg[i]==1) {
-			res[i]=-1;
-		}
-	}
-	res[count-1]=1;
-	return res;
-}
-
-bool check (vector <int> a, vector <int> b) {
-	for (int i=0;i<a.size() && i<b.size();i++) {
-		if (a[i]!=0 && b[i]!=0) {
-			return false;
-		}
-		else if (a[i]==0 && b[i]==0) {
-			return false;
-		}
-	}
-	if (a.size()>b.size()) {
-		for (int i=b.size();i<a.size();i++) {
-			if (a[i]==0) {
-				return false;
-			}
-		}
-	}
-	if (b.size()>a.size()) {
-		for (int i=a.size();i<b.size();i++) {
-			if (b[i]==0) {
-				return false;
-			} 
-		}
-	}
+	if ((x/2+y/2)%2==0) 
+		return false;
 	return true;
 }
 
-void printans(vector <int> a, vector <int> b, bool negx,bool negy) {
-	char east='E', west='W',north='N',south='S',temp;
-	if (negx==true) {
-		temp=west;
-		west=east;
-		east=temp;
-	}
-	if (negy==true) {
-		temp=south;
-		south=north;
-		north=temp;
-	}
-	for (int i=0;i<min(a.size(),b.size());i++) {
-		if (a[i]==1) {
-			cout<<east;
+int solve () {
+	int x,y;
+	scanf("%d %d",&x,&y);
+	bool impossible=false;
+	string ans;
+	while (impossible==false && (x!=0 || y!=0)) {
+		/*debug(x);
+		debug(y);*/
+		//debug((x+y)%2);
+		if ((x+y)%2==0 && (x!=0 || y!=0)) {
+			impossible=true;
+			break;
 		}
-		else if (a[i]==-1) {
-			cout<<west;
-		}
-		else if (b[i]==1) {
-			cout<<north;
+		if (x%2!=0) {
+			int count=0;
+			char move;
+			if (check (x-1,y)) {
+				move='E';
+				count++;
+			}
+			if (check(x+1,y)) {
+				move='W';
+				count++;
+			}
+			if (count==0) {
+				// cout<<"Entered here"<<endl;
+				impossible=true;
+				move='I';
+			}
+			if (count>1) {
+				if (abs(x+y+1)>(abs(x+y-1))) {
+					move='E';
+				}
+				else {
+					move='W';
+				}
+			}
+			if (move!='I') ans+=move;
+			if (move=='E') {
+				x--;
+			}
+			else if (move=='W') {
+				x++;
+			}
 		}
 		else {
-			cout<<south;
+			int count=0;
+			char move;
+			if (check(x,y-1)) {
+				move='N';
+				count++;
+			}
+			if (check(x,y+1)) {
+				move='S';
+				count++;
+			}
+			//debug(count);
+			if (count==0) {
+				// cout<<"Entered here"<<endl;
+				impossible=true;
+				move='I';
+			}
+			if (count==2) {
+				if (abs(x+y+1)>abs(x+y-1)) {
+					move='N';
+				}
+				else {
+					move='S';
+				}
+			}
+			if (move!='I') ans+=move;
+			if (move=='N') {
+				y--;
+			}
+			else if (move=='S') {
+				y++;
+			}
 		}
+		x=x/2;
+		y=y/2;
 	}
-	if (a.size()>b.size()) {
-		for (int i=b.size();i<a.size();i++) {
-			if (a[i]==1) {
-				cout<<east;
-			}
-			else if (a[i]==-1) {
-				cout<<west;
-			}
-		}
+	if (impossible==false) {
+		cout<<ans;
 	}
 	else {
-		for (int i=a.size();i<b.size();i++) {
-			if (b[i]==1) {
-				cout<<north;
-			}
-			else {
-				cout<<south;
-			}
-		}
+		cout<<"IMPOSSIBLE";
 	}
 }
 
-void testcase(int test) {
-	int x,y;
-	cin>>x>>y;
-	printf("Case #%d: ",test);
-	int xpos=0,ypos=0;
-	if ((x+y)%2==0) {
-		printf("IMPOSSIBLE");
-		return;
-	}
-	bool negx=false,negy=false;
-	if (x<0) {negx=true;x=-x;}
-	if (y<0) {negy=true;y=-y;}
-	if (x+y==5) {
-		if ((abs(x)==1 || abs(y)==1) && (abs(x)==4 || abs(y)==4)) {
-			char east='E', west='W',north='N',south='S',temp;
-			if (negx==true) {
-				temp=west;
-				west=east;
-				east=temp;
-			}
-			if (negy==true) {
-				temp=south;
-				south=north;
-				north=temp;
-			}
-			if (abs(x)==1) {
-				cout<<west<<east<<north;
-			}
-			else {
-				cout<<south<<north<<east;
-			}
-			return;
-		}
-	}
-	vector <int> xmoves=getbin(x);
-	vector <int> ymoves=getbin(y);
-	if (check(xmoves,ymoves)) {
-		printans(xmoves,ymoves,negx,negy);
-		return;
-	}
-	vector <int> yneg=getnegative(y);
-	if (check(xmoves,yneg)) {
-		printans(xmoves,yneg,negx,negy);
-		return;
-	}
-	vector <int> xneg=getnegative(x);
-	if (check(xneg,yneg)) {
-		printans(xneg,yneg,negx,negy);
-		return;
-	}
-	if (check(xneg,ymoves)) {
-		printans(xneg,ymoves,negx,negy);
-		return;
-	}
-	printf("IMPOSSIBLE");
-}
 
 int main() {
 	int t;
 	cin>>t;
 	for (int test=1;test<=t;test++) {
-		testcase(test);
+		printf("Case #%d: ",test);
+		solve();
 		printf("\n");
 	}
 }
--- 2020/Round 1/B/b.cpp
+++ 2020/Round 1/B/b.cpp
@@ -6,17 +6,79 @@
 #include<map>
 using namespace std;
 #define debug(x) cout<<#x<<" = "<<x<<endl;
+const int MX = 1e9;
 
-void testcase() {
+vector <pair<int,int>> iqueries;
+void init () {
+	iqueries.emplace_back(0,0);
+	iqueries.emplace_back(-MX/2,MX/2);
+	iqueries.emplace_back(-MX/2,-MX/2);
+	iqueries.emplace_back(MX/2,-MX/2);
+	iqueries.emplace_back(MX/2,MX/2);
+}
+
+int initialquery(int val) {
+	string response;
+	cout<<iqueries[val].first<<" "<<iqueries[val].second<<endl;
+	cin>>response;
+	while (response=="MISS") {
+		cout<<iqueries[++val].first<<" "<<iqueries[val].second<<endl;
+		cin>>response;
+	}
+	if (response=="CENTER") {
+		return 101;
+	}
+	else if (response=="HIT") {
+		return val;
+	}
+	else if (response=="WRONG") {
+		return 404;
+	}
+}
+
+pair<int,int> binarysearch (int a, int b, int parity) {
+	if (parity) {
+		return make_pair(a,(b-(b-a)/2));
+	}
+	else {
+		return make_pair(a+(b-a)/2,b);
+	}
+}
+
+//get the queries for x, y
+//should be easy
+int xquery (int x1, int x2) {
+	int parity=0;
+	pair<int,int> temp;
+
+	while (x1!=x2) {
+		binarysearch(x1,x2,parity);
+	}
+
+
+int testcase testcase(int a, int b) {
+	bool firsthit=false;
+	int initial;
+	while (firsthit==false) {
+		initial=initialquery(0);
+	}
+	if (initial==101 || initial==404) 
+		return initial;
 	
 }
 
 int main() {
 	int t;
 	cin>>t;
+	int a,b;
+	cin>>a>>b;
+	init();
+	int ans;
 	for (int test=1;test<=t;test++) {
 		printf("Case #%d: ",test);
-		testcase();
-		printf("\n");
+		ans=testcase(a,b);
+		if (ans==404) {
+			break;
+		}
 	}
 }
