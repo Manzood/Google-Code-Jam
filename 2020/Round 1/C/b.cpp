b867f7c9d6ceffe65d3b9e48559d3eb0b47654a5
Manzood <manzood.naqvi@gmail.com>
2020-09-23 21:41:42 +0530

    Added a  practice solution

--- 2020/Round 1/C/b.cpp
+++ 2020/Round 1/C/b.cpp
@@ -3,23 +3,53 @@
 #include<iostream>
 #include<algorithm>
 #include<set>
+#include<map>
 using namespace std;
 #define debug(x) cout<<#x<<" = "<<x<<endl;
+#define int long long
 
 void testcase() {
-	string digit;
-	string response;
-	int count=0;
-	long long num;
 	int u;
-	
+	cin>>u;
+	map <char, int> mp;
+	for (int i=0;i<10000;i++) {
+		string a;
+		int temp;
+		cin>>temp;
+		getchar();
+		cin>>a;
+		for (char x: a) {
+			mp[x]++;
+		}
+	}
+	vector <int> occurances;
+	for (auto x: mp) {
+		occurances.push_back(x.second);
+		// debug(x.first);
+		// debug(x.second);
+	}
+	string res;
+	sort(occurances.begin(),occurances.end());
+	for (auto x: mp) {
+		if (x.second==occurances[0])
+			res+=x.first;
+	}
+	for (int i=occurances.size()-1;i>0;i--) {
+		for (auto x: mp) {
+			if (x.second==occurances[i]) {
+				res+=x.first;
+				break;
+			}
+		}
+	}
+	cout<<res;
 }
 
-int main() {
+int32_t main() {
 	int t;
 	cin>>t;
 	for (int test=1;test<=t;test++) {
-		printf("Case #%d: ",test);
+		printf("Case #%lld: ",test);
 		testcase();
 		printf("\n");
 	}
